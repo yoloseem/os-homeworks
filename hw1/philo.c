98fb540909c1d102da61c153d7cf6556e5c046d6
Hyunjun Kim <yoloseem@gmail.com>
2015-05-07 12:16:28 +0900

    Disable DEBUG

--- hw1/philo.c
+++ hw1/philo.c
@@ -6,7 +6,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define DEBUG 1
+#define DEBUG 0
 #define debug_print(fmt, ...) \
     do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
 
