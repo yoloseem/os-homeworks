45cab6f0782c88491726331e94aae7e2f294cc50
Hyunjun Kim <yoloseem@gmail.com>
2015-05-13 21:07:19 +0900

    Define Process struct and declare array of Process

--- hw3/simul.c
+++ hw3/simul.c
@@ -1,6 +1,19 @@
 /* simul.c */
 #include <stdio.h>
 
+#define MAX_PROCESSES 10
+
+typedef struct Process {
+    // (Integer) Bursting time in milliseconds
+    int burstTime;
+    // (Integer) Priority (lower value = higher prioirty)
+    int priority;
+    // (Integer) Time at the process appeared/created
+    int startAt;
+} Process;
+Process procs[MAX_PROCESSES];
+
+
 int main (void) {
     printf("Hyunjun gonna start hw 3\n");
 
