c4d6ec6462f60f8ab37ce0bbe5bf26ac724adcbb
Hyunjun Kim <yoloseem@gmail.com>
2015-05-13 21:26:51 +0900

    Input process information by reading from given filename

--- hw3/simul.c
+++ hw3/simul.c
@@ -16,9 +16,34 @@ typedef struct Process {
 } Process;
 Process procs[MAX_PROCESSES];
 
+// (Integer) Number of process to be scheduled
+int n;
 
-int main (void) {
-    printf("Hyunjun gonna start hw 3\n");
+int main (int argc, char** argv) {
+    if (argc == 3) {
+        /* Input from text file
+         *     $ ./exename [filename] [policy]
+         *
+         * Text file must be in format of:
+         *     Each line contains: "[burst time] [priority] [start time]"
+         *     "0 0 0" indicates the end of the input */
+        FILE *inputFp = fopen(argv[1], "r");
+        while( inputFp ) {
+            fscanf(inputFp, "%d%d%d",
+                   &procs[n].burstTime, &procs[n].priority, &procs[n].startAt);
+            if (!(procs[n].burstTime | procs[n].priority | procs[n].startAt))
+                break;
+            n++;
+        }
+        if (!n) {
+            ERROREXIT();
+        }
+        printf("Scheduling %d processes...\n", n);
+    }
+    else {
+        printf("Execute the program in format of:\n");
+        printf("    $ %s [filename] [policy]\n", argv[0]);
+    }
 
     return 0;
 }
